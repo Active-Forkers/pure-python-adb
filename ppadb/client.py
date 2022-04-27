a82fbd8d3fb8f60d51ecd578542af04e58df4139
OxNinja <57400445+OxNinja@users.noreply.github.com>
2022-04-27 17:03:17 +0200

    Started typing the variables, will need time

--- ppadb/client.py
+++ ppadb/client.py
@@ -5,16 +5,17 @@ from ppadb.utils.logger import AdbLogging
 logger = AdbLogging.get_logger(__name__)
 
 class Client(Host):
-    def __init__(self, host='127.0.0.1', port=5037):
+    def __init__(self, host='127.0.0.1': str, port=5037: int) -> None:
         self.host = host
         self.port = port
 
-    def create_connection(self, timeout=None):
+    def create_connection(self, timeout=None: float) -> Connection:
+        # never used and should instead be a static method
         conn = Connection(self.host, self.port, timeout)
         conn.connect()
         return conn
 
-    def device(self, serial):
+    def device(self, serial: str):
         devices = self.devices()
 
         for device in devices:
