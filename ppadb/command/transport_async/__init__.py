425c0b3558e36fcf9704aa88f17de0507df7ff42
OxNinja <57400445+OxNinja@users.noreply.github.com>
2022-04-28 16:06:58 +0200

    Typings for command/transport_async

--- ppadb/command/transport_async/__init__.py
+++ ppadb/command/transport_async/__init__.py
@@ -2,15 +2,17 @@ import logging
 import re
 import time
 
+from ppadb.connection_async import ConnectionAsync
+
 
 class TransportAsync:
-    async def transport(self, connection):
+    async def transport(self, connection: ConnectionAsync) -> ConnectionAsync:
         cmd = f"host:transport:{self.serial}"
         await connection.send(cmd)
 
         return connection
 
-    async def shell(self, cmd, timeout=None):
+    async def shell(self, cmd: str, timeout=None) -> str:
         conn = await self.create_connection(timeout=timeout)
 
         cmd = f"shell:{cmd}"
@@ -20,7 +22,7 @@ class TransportAsync:
         await conn.close()
         return result.decode('utf-8')
 
-    async def sync(self):
+    async def sync(self) -> ConnectionAsync:
         conn = await self.create_connection()
 
         cmd = "sync:"
@@ -28,7 +30,7 @@ class TransportAsync:
 
         return conn
 
-    async def screencap(self):
+    async def screencap(self) -> bytes:
         async with await self.create_connection() as conn:
             cmd = "shell:/system/bin/screencap -p"
             await conn.send(cmd)
