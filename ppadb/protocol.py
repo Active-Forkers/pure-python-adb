7680eb6456716c6634734b589b96dc5883bbcef3
OxNinja <57400445+OxNinja@users.noreply.github.com>
2022-04-28 15:41:06 +0200

    Typings for protocol.py

--- ppadb/protocol.py
+++ ppadb/protocol.py
@@ -11,15 +11,15 @@ class Protocol:
     QUIT = 'QUIT'
 
     @staticmethod
-    def decode_length(length):
+    def decode_length(length: str) -> int:
         return int(length, 16)
 
     @staticmethod
-    def encode_length(length):
+    def encode_length(length: int) -> str:
         return f"{length:04X}"
 
     @staticmethod
-    def encode_data(data):
+    def encode_data(data: str) -> bytes:
         b_data = data.encode('utf-8')
         b_length = Protocol.encode_length(len(b_data)).encode('utf-8')
         return b"".join([b_length, b_data])
